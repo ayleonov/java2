973af0734912b64f7d63bce8d95196b2cce26b89
ayleonov <allesh002@mail.ru>
2020-09-07 00:02:13 +0300

    test

---
+++ src/ru/progwards/java2/lessons/test/Test1.java
@@ -0,0 +1,7 @@
+package ru.progwards.java2.lessons.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.print("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+    }
+}
